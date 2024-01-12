e447bb76a13e6c9db824db6787c8e2e6a54b1141
You-been <ybcho97@naver.com>
2024-01-12 09:33:07 +0900

    수정

--- project.js
+++ project.js
@@ -25,10 +25,10 @@ document.addEventListener("DOMContentLoaded", function () {
   // 스크롤 이벤트에 대한 핸들러
   document.addEventListener("scroll", function () {
     // 트리거 div의 위치 계산
-    const triggerDivBottom = triggerDiv.getBoundingClientRect().top;
+    const triggerDivTop = triggerDiv.getBoundingClientRect().top;
 
     // 스크롤 위치가 트리거 div의 상단에 도달하면 배경색을 변경
-    if (triggerDivBottom <= window.innerHeight) {
+    if (triggerDivTop <= window.innerHeight) {
       body.style.backgroundColor = "#eff0c1";
       menuActive.style.backgroundColor = "#eff0c1b3";
     } else {
